dc5b0ae53dc3dac36a576b716ae01ca730f35d3b
Yelena Strizhakova <e.strizhakova@gmail.com>
2025-02-20 18:26:07 +0400

    refactor: news cards

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 import "./NewsCardList.css";
 import { NewsCard, Button } from "../index";
-import { useLocation } from 'react-router-dom';
 
-const NewsCardList = ({ cards, title, doNeedBtn, main, onCardSave, onCardUnSave, onCardDelete }) => {
+
+const NewsCardList = ({
+  cards,
+  title,
+  doNeedBtn,
+  main,
+  onCardSave,
+  onCardUnSave,
+  onCardDelete,
+}) => {
   const [quantity, setQuantity] = React.useState(3);
-  const [width, setWidth] = React.useState(window.innerWidth);
   const [isBtnDisabled, setIsBtnDisabled] = React.useState(false);
   const currentPath = useLocation().pathname;
 
@@ -15,47 +24,22 @@ const NewsCardList = ({ cards, title, doNeedBtn, main, onCardSave, onCardUnSave,
     } else {
       setIsBtnDisabled(true);
     }
-  }, [quantity]);
+  }, [cards, quantity]);
+
 
   React.useEffect(() => {
-    const resize = () => {
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener("resize", resize);
-    return () => window.removeEventListener("resize", resize);
-  }, []);
-
-  let textLength;
-  let titleLength;
-
-  if (width <= 480) {
-    textLength = 90;
-    titleLength = 30;
-  } else if (width <= 768) {
-    textLength = 70;
-    titleLength = 25;
-  } else if (width <= 1024) {
-    textLength = 90;
-    titleLength = 17;
-  }else if (width <= 1280) {
-    textLength = 115;
-    titleLength = 25;
-  } else {
-    textLength = 157;
-    titleLength = 40;
-  }
-
-  React.useEffect(() => {
-    if (currentPath === '/saved-news' && cards !== null) {
+    if (currentPath === "/saved-news" && cards !== null) {
       setQuantity(cards.length);
     }
-  }, [cards])
+  }, [currentPath, cards]);
 
-  if (!cards || cards === null) { return null }
+  if (!cards || cards === null) {
+    return null;
+  }
   const cardsToRender = cards.slice(0, quantity);
 
   function showMoreCards() {
-    setQuantity(quantity + 3)
+    setQuantity(quantity + 3);
   }
 
   return (
@@ -66,16 +50,8 @@ const NewsCardList = ({ cards, title, doNeedBtn, main, onCardSave, onCardUnSave,
           <NewsCard
             key={item.url}
             img={item.urlToImage}
-            title={
-              item.title.length > titleLength
-                ? `${item.title.substring(0, titleLength)}...`
-                : `${item.title.substring(0, titleLength)}`
-            }
-            text={
-              item.description.length > textLength
-                ? `${item.description.substring(0, textLength)}...`
-                : `${item.description.substring(0, textLength)}`
-            }
+            title={item.title}
+            text={item.description}
             source={item.source.name}
             date={item.publishedAt}
             keyword={item.keyword}
@@ -89,7 +65,15 @@ const NewsCardList = ({ cards, title, doNeedBtn, main, onCardSave, onCardUnSave,
           />
         ))}
       </ul>
-      {doNeedBtn && <Button place={"search-results"} text={"Показать еще"} color={"white"} handleBtnClick={showMoreCards} isResultsBtnDisabled={isBtnDisabled} />}
+      {doNeedBtn && (
+        <Button
+          place={"search-results"}
+          text={"Показать еще"}
+          color={"white"}
+          handleBtnClick={showMoreCards}
+          isResultsBtnDisabled={isBtnDisabled}
+        />
+      )}
     </div>
   );
 };
