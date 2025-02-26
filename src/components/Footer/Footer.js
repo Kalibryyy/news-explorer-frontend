38ea1ddc48f875c28213fcbccb3b1d65c2e1bec3
Yelena Strizhakova <e.strizhakova@gmail.com>
2025-02-26 12:36:12 +0400

    refactor: social network links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css';
-import github from '../../images/github.svg';
-import fb from '../../images/fb.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Footer.css";
+import github from "../../images/github.svg";
+import linkedin from "../../images/linkedin.svg";
 
 function Footer() {
-    return (
-      <footer className="footer">
-        <div className="footer__links">
-          <nav className="footer__nav">
-            <ul className="footer__nav-list">
-              <li className="footer__nav-item"><Link to={''} className="footer__link">Главная</Link></li>
-              <li className="footer__nav-item"><a href="https://praktikum.yandex.ru/" target="_blank" className="footer__link">Яндекс.Практикум</a></li>
-            </ul>
-            <ul className="footer__nav-list">
-              <li className="footer__nav-item"><a href="https://github.com/Kalibryyy" target="_blank" className="footer__link"><img src={github} alt="логотип Гитхаба"/></a></li>
-              <li className="footer__nav-item"><a href="https://www.facebook.com/" target="_blank" className="footer__link"><img src={fb} alt="логотип Фэйсбука"/></a></li>
-            </ul>
-          </nav>
-        </div>
-      </footer>
-
-    );
+  return (
+    <footer className="footer">
+      <div className="footer__links">
+        <nav className="footer__nav">
+          <ul className="footer__nav-list">
+            <li className="footer__nav-item">
+              <Link to={""} className="footer__link">
+                Главная
+              </Link>
+            </li>
+            <li className="footer__nav-item">
+              <a
+                href="https://praktikum.yandex.ru/"
+                target="_blank"
+                className="footer__link"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+          </ul>
+          <ul className="footer__nav-list">
+            <li className="footer__nav-item">
+              <a
+                href="https://github.com/Kalibryyy"
+                target="_blank"
+                className="footer__link"
+              >
+                <img
+                  className="footer__nav-image"
+                  src={github}
+                  alt="логотип Гитхаба"
+                />
+              </a>
+            </li>
+            <li className="footer__nav-item">
+              <a
+                href="https://www.linkedin.com/in/elena-strizhakova/"
+                target="_blank"
+                className="footer__link"
+              >
+                <img
+                  className="footer__nav-image"
+                  src={linkedin}
+                  alt="логотип Линкедина"
+                />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
